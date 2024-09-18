dec45e87477bf0d5cffe12ffe131e6923b3c23ae
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-18 16:23:28 -0300

    Route delete

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ Route::post('/book-store', [BookController::class, 'store'])->name('book.store')
 Route::get('/book-show/{bookModel}', [BookController::class, 'show'])->name('book.show');
 Route::get('/book-edit/{bookModel}', [BookController::class, 'edit'])->name('book.edit');
 Route::put('/book-update/{bookModel}', [BookController::class, 'update'])->name('book.update');
+Route::delete('/book-destroy/{bookModel}', [BookController::class, 'destroy'])->name('book.destroy');
