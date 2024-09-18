cc836cf51050a270e8f84bdee033b7fef8e46291
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-18 15:37:07 -0300

    Method edit and update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -29,4 +29,20 @@ class BookController extends Controller
     public function show(BookModel $bookModel){
         return view('book.show', ['bookModel' => $bookModel]);
     }
+
+    public function edit(BookModel $bookModel){
+        return view('book.edit', ['bookModel' => $bookModel]);
+    }
+
+    public function update(BookRequest $bookRequest, BookModel $bookModel){
+        $bookRequest->validated();
+        
+        $bookModel->update([
+            'title' => $bookRequest->title,
+            'category' => $bookRequest->category,
+            'published' => $bookRequest->published,
+        ]);
+
+        return redirect()->route('book.show', ['bookModel' => $bookModel->id])->with('sucess', "Edição realizada com sucesso");
+    }
 }
