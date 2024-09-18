519ac85d12f04d310edc0ae36303f56b59ffcb3a
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-18 11:45:58 -0300

    Alter basic type of published

--- database/migrations/2024_09_18_142747_create_books.php
+++ database/migrations/2024_09_18_142747_create_books.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('category');
-            $table->string('published');
+            $table->integer('published');
             $table->timestamps();
         });
     }
