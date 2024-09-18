0058b4d1b238a73e2b1ef5c1ad451a20b4c861fa
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-18 00:36:23 -0300

    setting welcome route

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,14 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function (){
+    return view('welcome');
+});
+
+Route::controller(BookController::class)->group(function(){
+    Route::get('/books', 'index')->name('book.index');
+    Route::get('/book', 'create')->name('book.create');
+});
