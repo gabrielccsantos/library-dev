12b2e1ef52caa4d15a62d7383a69d503fb5b7c22
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-18 11:52:33 -0300

    Valited request and save in database and show in the view book.show

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\BookRequest;
+use App\Models\BookModel;
 
 class BookController extends Controller
 {
@@ -16,7 +17,15 @@ class BookController extends Controller
     }
 
     public function store(BookRequest $bookRequest){
-        $bookRequest->validated();
         
+        $bookRequest->validated();
+                
+        $bookModel = BookModel::create($bookRequest->all());
+
+        return redirect()->route('book.show', ['bookModel' => $bookModel->id])->with('sucess', "Cadastro realizado com sucesso");
+    }
+
+    public function show(BookModel $bookModel){
+        return view('book.show', ['bookModel' => $bookModel]);
     }
 }
