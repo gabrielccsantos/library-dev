831bd1463817af0a8ee6b7e54cb66fbfce2afc93
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-18 09:54:44 -0300

    Request validate created

---
+++ app/Http/Requests/BookRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'book_title' => 'required',
+            'book_category' => 'required',
+            'book_published' => 'required'
+        ];
+    }
+
+    public function mensages(): array
+    {
+        return [
+            'required.book_title' => 'Campo Título do livro é obrigatório',
+            'required.book_category' => 'Campo Categória do livro é obrigatório',
+            'required.book_published' => 'Campo Ano de Publicação é obrigatório'
+        ];
+    }
+}
