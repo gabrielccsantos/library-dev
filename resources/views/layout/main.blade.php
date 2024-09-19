8265ae7ab3c60dc34c3127488c4acabcb3d337e1
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-19 10:12:31 -0300

    Created layout

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Library Dev</title>
+</head>
+
+<body>
+@yield('content')
+<footer>
+    <p>Library Dev</p>
+</footer>
+</body>
+</html>
