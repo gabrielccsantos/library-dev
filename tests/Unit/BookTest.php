e68554a518562f7e38dd37ab95f5886cb704b4b6
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-20 16:38:50 -0300

    Created unit test

---
+++ tests/Unit/BookTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Http\Requests\BookRequest;
+use Illuminate\Support\Facades\Validator;
+
+class BookTest extends TestCase
+{
+      
+    public function test_book_valid_integer()
+    {
+        $data = ['title' => 'Livro Exemplo', 'category' => 'Ficção', 'published' => 'abc'];
+        $request = new BookRequest();
+        $validator = Validator::make($data, $request->rules());
+
+        $this->assertTrue($validator->fails());
+        $this->assertEquals('O campo ano deve conter apenas números.', $validator->errors()->first('published'));
+}
+
+    public function test_book_year_valid(){
+        $data = ['title' => 'Livro Exemplo', 'category' => 'Ficção', 'published' => 'abc'];
+        $request = new BookRequest();
+        $validator = Validator::make($data, $request->rules());
+
+        $data['published'] = '202';
+        $validator = Validator::make($data, $request->rules());
+        $this->assertTrue($validator->fails());
+        $this->assertEquals('O campo ano deve ter exatamente 4 dígitos.', $validator->errors()->first('published'));
+
+        $data['published'] = '2023';
+        $validator = Validator::make($data, $request->rules());
+        $this->assertFalse($validator->fails());
+
+        $data['published'] = '202';
+        $validator = Validator::make($data, $request->rules());
+        $this->assertTrue($validator->fails());
+        $this->assertEquals('O campo ano deve ter exatamente 4 dígitos.', $validator->errors()->first('published'));
+    
+    }
+}
