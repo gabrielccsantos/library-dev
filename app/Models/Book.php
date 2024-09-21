a96dbb7aca12cc0c7a8b47e532ae46a30c70eae7
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-21 11:32:20 -0300

    Change name of model to Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BookModel extends Model
+class Book extends Model
 {
     use HasFactory;
 
-    protected $table = 'books';
-    protected $primaryKey = 'id';
     protected $fillable = ['title', 'category', 'published'];
 }
