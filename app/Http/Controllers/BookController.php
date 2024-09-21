5441949a1912da1452373c8555e1bf7ca40536ed
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-21 11:31:40 -0300

    Alter name of Model to Book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\BookRequest;
-use App\Models\BookModel;
+use App\Models\Book;
 
 class BookController extends Controller
 {
     public function index()
     {
-        $books = BookModel::orderBy('created_at')->get();
+        $books = Book::orderBy('created_at')->get();
         return view('book.index', ['books' => $books]);
     }
 
@@ -23,37 +23,37 @@ class BookController extends Controller
         
         $bookRequest->validated();
                 
-        $bookModel = BookModel::create($bookRequest->all());
+        $book = Book::create($bookRequest->all());
 
-        return redirect()->route('book.show', ['bookModel' => $bookModel->id])->with('sucess', "Cadastro realizado com sucesso");
+        return redirect()->route('book.show', ['book' => $book->id])->with('sucess', "Cadastro realizado com sucesso");
     }
 
-    public function show(BookModel $bookModel)
+    public function show(Book $book)
     {
-        return view('book.show', ['bookModel' => $bookModel]);
+        return view('book.show', ['book' => $book]);
     }
 
-    public function edit(BookModel $bookModel)
+    public function edit(Book $book)
     {
-        return view('book.edit', ['bookModel' => $bookModel]);
+        return view('book.edit', ['book' => $book]);
     }
 
-    public function update(BookRequest $bookRequest, BookModel $bookModel)
+    public function update(BookRequest $bookRequest, Book $book)
     {
         $bookRequest->validated();
         
-        $bookModel->update([
+        $book->update([
             'title' => $bookRequest->title,
             'category' => $bookRequest->category,
             'published' => $bookRequest->published,
         ]);
 
-        return redirect()->route('book.show', ['bookModel' => $bookModel->id])->with('sucess', "Edição realizada com sucesso");
+        return redirect()->route('book.show', ['book' => $book->id])->with('sucess', "Edição realizada com sucesso");
     }
 
-    public function destroy(BookModel $bookModel)
+    public function destroy(Book $book)
     {
-        $bookModel->delete();
+        $book->delete();
 
         return redirect()->route('book.index')->with('sucess', "Livro excluído com sucesso");
     }
