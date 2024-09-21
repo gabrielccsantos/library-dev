7d3ba4ef219c3f16d38f4e604e8c28ead6336419
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-21 14:58:12 -0300

    Forein key

--- database/migrations/2024_09_21_144755_create_books.php
+++ database/migrations/2024_09_21_144755_create_books.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('published');
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
 
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
