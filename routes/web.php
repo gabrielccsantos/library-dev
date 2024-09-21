425c404d857666ed7f4e349776b30b12e48a4098
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-21 13:06:40 -0300

    Created routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,4 +17,14 @@ Route::controller(BookController::class)->group(function(){
     Route::get('/books/{book}/edit', 'edit')->name('book.edit');
     Route::put('/books/{book}/', 'update')->name('book.update');
     Route::delete('/books/{book}', 'destroy')->name('book.destroy');
+});
+
+Route::controller(CategoryController::class)->group(function(){
+    Route::get('/categories', 'index')->name('category.index');
+    Route::get('/categories/create', 'create')->name('category.create');
+    Route::post('/categories', 'store')->name('category.store');
+    Route::get('/categories/{category}', 'show')->name('category.show');
+    Route::get('/categories/{category}/edit', 'edit')->name('category.edit');
+    Route::put('/categories/{category}/', 'update')->name('category.update');
+    Route::delete('/categories/{category}', 'destroy')->name('category.destroy');
 });
