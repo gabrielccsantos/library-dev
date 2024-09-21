d0dce5615f2f6590b6140ba80dcc5e90224400ea
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-21 14:37:14 -0300

    Relationship composicion

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Book extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'published'];
+
+    public function category(): HasOne
+    {
+        return $this->hasOne(Category::class);
+    }
 }
