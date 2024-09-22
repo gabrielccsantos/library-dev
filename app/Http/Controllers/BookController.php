7761f91823d21c6ee27507b167168205d4ccd8d6
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-22 17:55:27 -0300

    join array authors in book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\BookRequest;
 use App\Models\Category;
 use App\Models\Book;
+use App\Models\Author;
 
 class BookController extends Controller
 {
@@ -16,8 +17,8 @@ class BookController extends Controller
 
     public function create(){
         $categories = Category::all();
-
-        return view('book.create', compact('categories'));
+        $authors = Author::all();
+        return view('book.create', compact('categories', 'authors'));
     }
 
     public function store(BookRequest $bookRequest)
@@ -26,6 +27,8 @@ class BookController extends Controller
         $bookRequest->validated();
                 
         $book = Book::create($bookRequest->all());
+
+        $book->authors()->attach($bookRequest->input('authors'));
 
         return redirect()->route('book.show', ['book' => $book->id])->with('sucess', "Cadastro realizado com sucesso");
     }
@@ -38,7 +41,8 @@ class BookController extends Controller
     public function edit(Book $book)
     {
         $categories = Category::all();
-        return view('book.edit', ['book' => $book, 'categories' => $categories]);
+        $authors = Author::all();
+        return view('book.edit', ['book' => $book, 'categories' => $categories, 'authors' => $authors]);
     }
 
     public function update(BookRequest $bookRequest, Book $book)
@@ -50,6 +54,8 @@ class BookController extends Controller
             'published' => $bookRequest->published,
             'category_id' => $bookRequest->category_id,
         ]);
+
+        $book->authors()->sync($bookRequest->authors);
 
         return redirect()->route('book.show', ['book' => $book->id])->with('sucess', "Edição realizada com sucesso");
     }
