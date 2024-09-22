dde9b5f9b4d806d514ae23b90f665d1901d132f7
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-22 11:23:27 -0300

    Change delete logic

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -50,12 +50,8 @@ class AuthorController extends Controller
 
     public function destroy(Author $author)
     {
-        if ($author->books()->count() > 0) {
-            return redirect()->route('author.index')->with('unsucess', 'Não é possível excluir a categoria. Existem livros relacionados a ela.');
-        }
-
         $author->delete();
 
-        return redirect()->route('author.index')->with('sucess', "Categoria excluída com sucesso");
+        return redirect()->route('author.index')->with('sucess', "Autor excluído com sucesso");
     }
 }
