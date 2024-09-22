97f31d299f871376199913383b8d3ff1cf5eeffa
Gabriel Santos <gabrielcarneirocsantos42@gmail.com>
2024-09-22 10:24:22 -0300

    Add HasMany in function return

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -12,7 +13,7 @@ class Category extends Model
 
     protected $fillable = ['name'];
 
-    public function books()
+    public function books(): HasMany
     {
         return $this->hasMany(Book::class);
     }
